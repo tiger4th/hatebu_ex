819911aaf97a7038575a3b2f4eee46ed16248b26
tiger4th <tiger4thg@gmail.com>
2026-04-17 00:41:11 +0900

    404

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php http_response_code(404); ?>
 <?php require("./logic.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
